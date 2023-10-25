29367a769f5b27b91fd26651543d3bfed90fe509
Nick Badal <me@nbad.al>
2023-10-25 03:31:08 +0000

    Test configuration

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "nbadal"
 
     workspaces {
       name = "homelab-external"
